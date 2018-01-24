d8d8c32a0759cc98dbb20e922c57e89e337888c3
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-01-24 11:28:36 +0530

    sdsdsdsa

--- sample222222/ConnectionManager.js
+++ sample222222/ConnectionManager.js
@@ -9,5 +9,12 @@ module.exports=function(){
         password: "12345678",
         database: "mobileBackend",
     };
+    this.dbConnections["test3"] = {
+        host: "test3.cvmlduwb8e2g.us-east-1.rds.amazonaws.com",
+        port: "3306",
+        user: "rdsuser",
+        password: "12345678",
+        database: "firstDB",
+    };
     
     };
--- sample222222/lambda.js
+++ sample222222/lambda.js
@@ -4,24 +4,11 @@ let SL = require('@slappforge/slappforge-sdk');
 const rds = new SL.AWS.RDS(connectionManager);
 exports.handler = function (event, context, callback) {
 
-	// Replace the query with the actual query
-	// You can pass the existing connection to this function.
-	// A new connection will be created if it's not present as the third param 
-	// You must always end the DB connection after it's used
-	rds.query({
-		instanceIdentifier: 'mobilebackend',
-		query: 'asasa',gfgfgh
-		inserts: []
-	}, function (error, results, connection) {
-		if (error) {hh
-			console.log("Error occurred");
-			throw error;
-		} else {
-			console.log("Success")
-			console.log(results);
-		}
-sdsadasdadsdas
-		connection.end();
+
+	rds.beginTransaction({
+		instanceIdentifier: 'test3'
+	}, function (error, connection) {
+		if (error) { throw err; }
 	});
 
 
